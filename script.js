f28040e56b212f2b92efeb06776d929b6b76fd70
dekapaan <terblancheronald@gmail.com>
2021-05-09 22:51:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+new fullpage("#fullpage", {
+  //options here
+  menu: "#menu",
+  anchors: ["firstPage", "secondPage", "thirdPage", "fourthPage"],
+  autoScrolling: false,
+  scrollOverflow: true,
+  navigation: false,
+  fitToSection: false,
+  scrollVertically: true,
+  continuousVertical: true,
+  navigationPosition: "right",
+  easing: "easeInOutCubic",
+  easingcss3: "ease",
+});
