ae5b78b2585278b0d332f5ead469a42471828862
dekapaan <terblancheronald@gmail.com>
2021-05-10 16:37:07 +0200

    aweh

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+new fullpage("#fullpage", {
+  //options here
+  menu: "#myMenu",
+  anchors: ["firstPage", "secondPage", "thirdPage", "fourthPage"],
+  lockAnchors: false,
+  autoScrolling: true,
+  scrollOverflow: true,
+  navigation: false,
+  fitToSection: false,
+  scrollVertically: true,
+  continuousVertical: true,
+  navigationPosition: "right",
+});
